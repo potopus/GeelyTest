150bcbba7d6b65532f12810cc61755ac3bc31000
potopus <paveldelo@yandex.ru>
2023-06-22 17:51:43 +0300

    feat: compleate test template in photoshop

---
+++ js/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+    $(".slider").slick({
+        arrows: false,
+        dots: true,
+        variableWidth: true,
+        initialSlide: 4,
+    });
+    $(".slider-vert").slick({
+        // variableWidth:true,
+        // variableWidth: true,
+        vertical: true,
+        verticalSwiping: true,
+        slidesToShow: 3,
+        responsive: [
+            {
+                breakpoint: 1280,
+                slidesToShow: 3,
+            }
+
+        ]
+    });
+})
+
+// Interpolation
+
+// const slider = document.querySelector(".slider");
+
+// window.addEventListener(`resize`, event => {
+//     const windowInnerWidth = window.innerWidth;
+//     let intrepolation =   
+//     // some code
+//   }, false);
